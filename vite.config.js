1b31cde2e1426179b7dc8f69da072b30cebd3921
iamurpookie6969-oss <i.amur.pookie6969@gmail.com>
2026-04-27 22:08:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
+export default {
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
+};
