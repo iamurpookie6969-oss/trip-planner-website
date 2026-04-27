643ff77ba8e1273eb9cdf3e7e5e61ac99028aa70
iamurpookie6969-oss <i.amur.pookie6969@gmail.com>
2026-04-27 22:13:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Landing from "./pages/Landing";
-import { CreateTrip } from "./pages/create-trips/CreateTrip";
-import { MyTrips } from "./pages/my-trips/MyTrips";
-
 function App() {
-  return (
-    <Router>
-      <Routes>
-
-        {/* 🔥 NEW LANDING PAGE */}
-        <Route path="/" element={<Landing />} />
-
-        {/* EXISTING FEATURES */}
-        <Route path="/create-trip" element={<CreateTrip />} />
-        <Route path="/my-trips" element={<MyTrips />} />
-
-      </Routes>
-    </Router>
-  );
+  return <h1 style={{textAlign:"center"}}>App Working ✅</h1>;
 }
 
 export default App;
