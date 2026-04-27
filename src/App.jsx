b75fa8d52ad15422ff417fb38474bc167683b8ca
iamurpookie6969-oss <i.amur.pookie6969@gmail.com>
2026-04-27 22:19:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Hero } from "./components/dashboard/Hero";
+import { Navbar } from "./components/common/Navbar";
+
 function App() {
-  return <h1 style={{textAlign:"center"}}>App Working ✅</h1>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
