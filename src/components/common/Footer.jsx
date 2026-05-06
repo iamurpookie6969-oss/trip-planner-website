47f1790e2c5b19b2b51c78dc0b13034c5b9f5944
iamurpookie6969-oss <i.amur.pookie6969@gmail.com>
2026-05-06 13:31:40 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     <footer className="bg-background rounded-lg shadow ">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="block text-sm text-muted-foreground-500 sm:text-center dark:text-muted-foreground-400">
           © 2024{" "}
           <Link to={"/"} className="hover:underline">
             Trip Planner™
