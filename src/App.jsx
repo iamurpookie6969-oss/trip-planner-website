cae2a7392e97736f5c652531cfd73e948fc2b12e
iamurpookie6969-oss <i.amur.pookie6969@gmail.com>
2026-05-06 14:16:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/common/Navbar";
-import { Hero } from "./components/dashboard/Hero";
 
 function App() {
   return (
     <div className="bg-background text-foreground min-h-screen">
       <Navbar />
-
-      {/* If using routing layout */}
       <Outlet />
-      <Hero />
-
-      {/* OR if homepage */}
-      {/* <Hero /> */}
     </div>
   );
 }
