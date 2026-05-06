304d42a62c4a2ec45bcd820181fef183c2e09c59
iamurpookie6969-oss <i.amur.pookie6969@gmail.com>
2026-05-06 12:52:24 +0530

    Update MyTrips.jsx

--- src/pages/my-trips/MyTrips.jsx
+++ src/pages/my-trips/MyTrips.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 export const MyTrips = () => {
   const navigate = useNavigate();
   const [userTrips, setUserTrips] = useState([]);
+
   const getUsertrips = async () => {
     const user = JSON.parse(localStorage.getItem("user"));
     if (!user) {
@@ -19,34 +20,39 @@ export const MyTrips = () => {
       collection(db, "trips"),
       where("userEmail", "==", user?.email)
     );
+
     const querySnapshot = await getDocs(q);
     setUserTrips([]);
+
     querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      //   console.log(doc.id, " => ", doc.data());
       setUserTrips((prev) => [...prev, doc.data()]);
     });
   };
-  console.log(userTrips);
+
   useEffect(() => {
     getUsertrips();
   }, []);
+
   return (
-    <div>
+    <div className="bg-background text-foreground min-h-screen">
       <Navbar />
+
       <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
-        <h2 className="font-bold text-2xl">My Trips</h2>
+        <h2 className="font-bold text-2xl text-foreground">My Trips</h2>
+
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-4">
-          {userTrips?.length > 0
-            ? userTrips.map((item, index) => (
-                <MyTripCard item={item} index={index} />
-              ))
-            : [1, 2, 3].map((item, index) => (
-                <div
-                  key={index}
-                  className="h-[250px] w-full bg-slate-200 animate-pulse rounded-md"
-                ></div>
-              ))}
+          {userTrips?.length > 0 ? (
+            userTrips.map((item, index) => (
+              <MyTripCard key={index} item={item} index={index} />
+            ))
+          ) : (
+            [1, 2, 3].map((item, index) => (
+              <div
+                key={index}
+                className="h-[250px] w-full bg-muted animate-pulse rounded-md"
+              ></div>
+            ))
+          )}
         </div>
       </div>
     </div>
