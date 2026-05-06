5dfa90aeb5c136485e3ca5616704f23369b217c8
iamurpookie6969-oss <i.amur.pookie6969@gmail.com>
2026-05-06 13:55:31 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="bg-background rounded-lg shadow ">
+    <footer className="bg-background border-t border-border">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-muted-foreground-500 sm:text-center dark:text-muted-foreground-400">
+
+        <hr className="my-6 border-border sm:mx-auto lg:my-8" />
+
+        <span className="block text-sm text-muted-foreground sm:text-center">
           © 2024{" "}
-          <Link to={"/"} className="hover:underline">
+          <Link to="/" className="hover:underline font-medium text-foreground">
             Trip Planner™
           </Link>
           . All Rights Reserved.
         </span>
+
       </div>
     </footer>
   );
