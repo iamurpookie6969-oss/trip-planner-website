6626927617da518d8e5bb113e6b2a934e52864d8
iamurpookie6969-oss <i.amur.pookie6969@gmail.com>
2026-05-06 13:38:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import { Navbar } from "./components/common/Navbar";
 function App() {
   return (
     <div className="bg-background text-foreground min-h-screen transition-colors duration-300">
-      {/* Navbar always visible */}
       <Navbar />
-
       <Outlet />
     </div>
   );
